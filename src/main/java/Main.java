5c9e4fcd5801948fd0d6e559d88b74aef00f0755
Anastasiya Saakyan <vv999vv@bk.ru>
2022-11-30 16:34:50 +0300

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
-import java.io.File;
-import java.util.Arrays;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -9,5 +10,18 @@ public class Main {
         // здесь создайте сервер, который отвечал бы на нужные запросы
         // слушать он должен порт 8989
         // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
+        try (ServerSocket serverSocket = new ServerSocket(8989)) {
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+                    String word = bufferedReader.readLine();
+                    printWriter.println(engine.search(word));
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("не могу стартовать сервер");
+            e.printStackTrace();
+        }
     }
 }
